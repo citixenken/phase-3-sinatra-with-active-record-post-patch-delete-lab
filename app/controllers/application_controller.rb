67296ffd3297bdff07301629a37a9ec78d599069
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-08-30 17:32:03 +0300

    COMPLETED: CRUD lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,34 @@ class ApplicationController < Sinatra::Base
     baked_good.to_json
   end
 
+  #POST /baked_goods
+  post '/baked_goods' do
+    baked_good = BakedGood.create(
+      name: params[:name],
+      price: params[:price],
+      bakery_id: params[:bakery_id]
+    )
+    baked_good.to_json
+  end
+
+  #PATCH /bakeries/:id
+  patch '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.update(
+      name: params[:name]
+    )
+    bakery.to_json
+  end
+
+  #DELETE /baked_goods/:id
+  delete '/baked_goods/:id' do 
+    # find baked_good using ID
+    baked_good = BakedGood.find(params[:id])
+
+    #delete baked_good
+    baked_good.destroy
+
+    #send response with delete baked_good as response
+    baked_good.to_json
+  end
 end
